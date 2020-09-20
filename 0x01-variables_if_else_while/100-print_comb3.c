4e09f802381be49f21a073cdb343c13fc2c519ce
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-20 03:24:23 +0000

    Solving 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,21 @@
 int main(void)
 {
 	int i, j;
+
 	for (i = 48; i < 58; i++)
 	{
 		for (j = i + 1; j < 58; j++)
 		{
-      if (i != j && i < j)
-      putchar(i);
-			putchar(j);
-      if (i == 56 && j == 57)
-      continue;
-      else
-      putchar(44);
-			putchar(32);
+		if (i != j && i < j)
+		putchar(i);
+		putchar(j);
+		if (i == 56 && j == 57)
+		continue;
+		else
+		putchar(44);
+		putchar(32);
 		}
 	}
-	putchar(10);
-	return (0);
+putchar(10);
+return (0);
 }
