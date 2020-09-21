bcb55783e988f37231e88b63f6b4b73030419d38
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-21 04:16:31 +0000

    Solving 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Check sign on int
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int i , j;
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(32);
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 98 && j == 99)
+			continue;
+			else
+			putchar(44);
+			putchar(32);
+		}
+	}
+putchar(10);
+return 0;
+}
