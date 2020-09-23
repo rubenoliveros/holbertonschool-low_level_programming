4b7fe926bc0ad0f0b334eacc31098b07c605050e
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-23 04:38:01 +0000

    Solving 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isalpha - a function that checks for alphabetic character.
+ *
+ *
+ * Return: 1 if alpha, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
