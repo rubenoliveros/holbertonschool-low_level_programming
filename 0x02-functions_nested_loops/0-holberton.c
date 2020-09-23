7e93576d193508ad7c9925075008fd33670357f4
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-23 02:02:08 +0000

    Solving 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
-/*
-* main
-*
-*  Return: 0
-*/
-int main(void) 
+#include "holberton.h"
+/**
+ * main - using en external function
+ *
+ *
+ * Return: 0
+ */
+int main(void)
 {
-	puts("Holberton");
-	return 0;
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
 }
