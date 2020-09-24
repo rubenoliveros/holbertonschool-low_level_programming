1fa603cc9d2a49fb5c019207804c53ac09760a0a
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-24 04:59:12 +0000

    Solving 0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - tests function for positive_or_negative.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
