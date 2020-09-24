b6535e353920cfdf0acc1e0305e5527611f16e4d
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-24 03:26:26 +0000

    Solving 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ * @i: 1st integer
+ * @j: 2nd integer
+ * Return: i + j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
