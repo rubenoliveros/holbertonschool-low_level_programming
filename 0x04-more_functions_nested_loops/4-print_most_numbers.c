16ebce7f51531e0496a3a6e138a084f63fa001b4
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-25 03:43:12 +0000

    Solving 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - a function that prints the numbers,
+ *                      from 0 to 9, followed by a new line.
+ *                      Do not print 2 and 4.
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		continue;
+		else
+		_putchar(i +'0');
+	}
+	_putchar('\n');
+}
