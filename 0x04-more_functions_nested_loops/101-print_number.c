fde28f20112662d03090b01fe12ab042af49502c
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-25 04:59:19 +0000

    Solving 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,13 +15,20 @@ void print_number(int n)
 	}
 
 	if (n >= 10000)
+	{
 		_putchar(((n / 10000)  % 10) + '0');
+	}
 	if (n >= 1000)
+	{
 		_putchar(((n / 1000)  % 10) + '0');
+	}
 	if (n >= 100)
+	{
 		_putchar(((n / 100)  % 10) + '0');
+	}
 	if (n >= 10)
+	{
 		_putchar(((n / 10)  % 10) + '0');
-
+	}
 	_putchar(n % 10 + '0');
 }
