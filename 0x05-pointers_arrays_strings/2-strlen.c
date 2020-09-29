2e3bfc218eb5d7523ea7d2392d96f4a08119c52f
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-29 16:23:30 +0000

    Solving 0x03-debugging

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ *
+ * @s: string input
+ *
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
