fe5ac0f254a0fdc749a0b8ecffbd60861f33de66
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-29 19:05:34 +0000

    Solving 0x03-debugging

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ *
+ * @str: variable
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
