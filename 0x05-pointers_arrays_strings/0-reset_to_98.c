f90cc22e44bf591d293d42eb80508ab16d38a821
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-29 13:40:14 +0000

    Solving 0x03-debugging

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 -  a function that takes a pointer to an int as
+ *                parameter and updates the value it points to to 98.
+ *
+ * @n: position in array to reset
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
