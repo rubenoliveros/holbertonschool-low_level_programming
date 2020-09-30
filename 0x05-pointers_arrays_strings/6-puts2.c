d4d9c7d0617e3efa4ec1b5d8a20fa1ce5054d249
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-30 01:48:11 +0000

    Solving 0x03-debugging

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * puts2 - a function that prints every other character of a string,
+ *         starting with the first character, followed by a new line.
+ *
+ * @str: variable
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
