d316b1837ee9782a2c496a0cc3d7709a71803f7f
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-09-30 03:35:29 +0000

    Solving 0x03-debugging

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* *_strcpy - a function that copies the string pointed to by src,
+*            including the terminating null byte (\0), to the buffer pointed to by dest.
+*
+* @dest: 1st variable
+* @src: 2nd variable
+*
+* Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int counter = 0;
+	int i;
+
+	while (src[counter] != 0)
+		counter++;
+	for (i = 0; i < counter; i++)
+		dest[i] = src[i];
+	return (dest);
+}
