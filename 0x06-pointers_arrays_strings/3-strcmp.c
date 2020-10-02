db4d2358c9c7f3a929c2baa760449c5e08c2d75a
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-10-02 03:30:44 +0000

    Solving 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+	}
+
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
