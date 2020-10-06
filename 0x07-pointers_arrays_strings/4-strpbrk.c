32924050debcf2766e7c065d49f072cd4ecbbf9e
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-10-06 04:47:18 +0000

    Solving 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes.
+ *
+ * @s: 1st variable
+ * @accept: 2nd variable
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s + i);
+	}
+	return (0);
+}
