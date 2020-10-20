921447c51023f24c20c6234d0339ea9733d16024
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-10-20 02:45:38 +0000

    Solving 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - information about dogs
+ *
+ * @name: 1st variable
+ * @age: 2nd variable
+ * @owner: 3rd variable
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H_ */
