4c7d76d777337a432a0a073823647435b38fbaf6
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-10-29 01:22:18 +0000

    Solving 0x10-variadic_functions

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,12 +1,13 @@
 section .text
 	global main
 main:
+	mov eax,4
 	mov ebx,1
 	mov ecx,msg
-	mov edx,17
-	mov eax,4
+	mov edx,msg_length
 	int 0x80
 	mov eax,0
 	int 0x80
 section .data
 	msg db 'Hello, Holberton',0xa
+	msg_length equ $-msg
