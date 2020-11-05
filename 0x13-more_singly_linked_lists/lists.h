b64cfc3b2325e26f400a5c73550a341d0ae8b5f5
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-11-05 03:31:19 +0000

    Solving 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,21 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-int _putchar(char c);
+#include <stddef.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -18,18 +32,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-#endif /* _LIST_H_ */
+#endif /* _LISTS_H_ */
