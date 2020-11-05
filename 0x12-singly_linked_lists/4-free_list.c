765add6fccf9ff319a0b88823eb91df3c0b66e1b
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-11-05 04:13:49 +0000

    Solving 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,11 +9,13 @@
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
+	list_t *tmp;
 
-	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
