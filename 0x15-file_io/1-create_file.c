eaa902f68e28111cea0309025597c38d5fddb289
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-11-10 04:59:45 +0000

    Solving 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,9 +2,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "holberton.h"
 
 /**
- * Helper function
  * _strlen - funcntion to find the length of a string
  * @str: pointer to the string
  *
@@ -32,6 +32,9 @@ int create_file(const char *filename, char *text_content)
 	ssize_t len = 0;
 
 	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 		len = write(fd, text_content, _strlen(text_content));
