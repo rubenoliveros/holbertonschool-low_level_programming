06db6c006e5649ad30d215af2ca548deb7385dfe
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-11-10 04:31:31 +0000

    Solving 0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stats.h>
+#include <fcntl.h>
+#include "holberton.h"
+
+/**
+ * read_textfile - Write a function that reads a text file and prints it to the
+ *                 POSIX standard output
+ * @filename: name of the file to read
+ * @letters:  number of letters it should read and print
+ *
+ * Return: number of letters used
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
+
+
